06574b40d42069350b843adc2bc731d7a9d44cd9
J4226 <j.acharya4226@student.leedsbeckett.ac.uk>
2019-10-23 16:04:37 +0000

    Episode 19

--- app/Task.php
+++ app/Task.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $guarded = [];
+    
+    public function complete($completed = true)
+    {
+        $this->update(['completed' => $completed]);
+    }
+    
+    
+    
     public function project()
     {
         return $this->belongsTo(Project::class);
