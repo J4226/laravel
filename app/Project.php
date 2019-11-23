72c40563c4e87bf0cfd352a26e7b94ac6fb94164
j4226 <j.acharya4226@student.leedsbeckett.ac.uk>
2019-11-23 19:20:02 +0000

    Episode 31

--- app/Project.php
+++ app/Project.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Mail\ProjectCreated;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
@@ -12,6 +14,18 @@ class Project extends Model
     
     ];
     
+    
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::created(function($project)
+        {
+         Mail::to($project->owner->email)->send(
+            new ProjectCreated($project)
+       );   
+        });
+    }
     public function owner()
     {
         return $this->belongsTo(User::class);
