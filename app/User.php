722f00cd13278221b39d3c95dbcb5cd087d944d4
j4226 <j.acharya4226@student.leedsbeckett.ac.uk>
2019-11-24 05:06:03 +0000

    ER Episode 2

--- app/User.php
+++ app/User.php
@@ -51,4 +51,16 @@ class User extends Authenticatable
     {
         return $this->hasOne(Experience::class);
     }
+    
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+    
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+    
+    
 }
